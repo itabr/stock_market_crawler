b5e0ae8ca74bf25a3017cfbf9b459d2bbbeffec8
itabr <itabrz@gmail.com>
2017-11-03 19:57:01 -0700

    stock_crawler.py added

---
+++ stock_crawler.py
@@ -0,0 +1,35 @@
+import tweepy
+
+consumer_key = "WMBaLdmJmWysb7kisqdKBD3Wd"
+consumer_secret = "mrCiHxnSv7ZV5HsC1Sywu44N7R0n3x4d4wmiSTszNt50Gq5ObX"
+access_token = "926633743841771520-gcJR5HWYH5pcneR0gJnP73jxuFdsgKC"
+access_token_secret = "4iNT66uZmufHLh79wkhz4WyI7tW0TrGkWW4auxRczbLcX"
+
+class listener(tweepy.StreamListener):
+	def on_data(self, data):
+		print(data)
+		return(True)
+
+	def on_error(self, status):
+		print(status)
+
+
+
+if __name__ == "__main__":
+
+	stock_name = "AAPL"
+
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	api = tweepy.API(auth)
+
+	# stream data
+
+	twitterStream = tweepy.Stream(auth, listener())
+	twitterStream.filter(track=[stock_name])
+
+	# request tweet
+
+	# r = api.search(q="TWTR")
+	# for tweet in r:
+	# 	print(tweet._json)
