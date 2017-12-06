725c854493d42746c970d074182e66c02f8f3142
itabr <itabrz@gmail.com>
2017-12-05 19:33:33 -0800

    stop word updated

--- code/stop_word.py
+++ code/stop_word.py
@@ -1,5 +1,22 @@
 from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
-
-def swremover(text):
-	text = ' '.join([word for word in text.split() if word not in ENGLISH_STOP_WORDS])
+import re
+def swremover(tweet):
+	text = ' '.join([word for word in tweet.split() if word not in ENGLISH_STOP_WORDS])
 	return text
+
+def replaceTwoOrMore(tweet):
+    #look for 2 or more repetitions of character and replace with the character itself
+    pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
+    return pattern.sub(r"\1\1", tweet)
+
+def getFeatureVector(tweet):
+	featureVector = []
+	tweet = swremover(tweet)
+	words = tweet.split()
+	for w in words:
+		w = replaceTwoOrMore(w)
+		w = w.strip('\'"?,.')
+		val = re.search(r"^[a-zA-Z][a-zA-Z0-9]*$", w)
+
+		featureVector.append(w.lower())
+	return featureVector
