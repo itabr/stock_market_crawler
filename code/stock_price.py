b2e0479004112e8254195209c3478a411ec04eb2
Nilesh C. Gupta <nilesh@risksciences.ucla.edu>
2017-12-09 02:29:24 -0800

    Built Linear Regressor to predict stock price

--- code/stock_price.py
+++ code/stock_price.py
@@ -3,6 +3,8 @@ import io
 import requests
 import time
 from datetime import datetime
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
  
 def google_stocks(symbol, startdate = (1, 1, 2006), enddate = None):
  
@@ -28,6 +30,8 @@ def google_stocks(symbol, startdate = (1, 1, 2006), enddate = None):
 if __name__ == '__main__':
     tesla_data = google_stocks('TSLA', startdate=(11, 28, 2017), enddate=(12, 11, 2017))
     tesla_data = tesla_data[['Date', 'Close']]
+    # For a given data, this is tomorrow's stock price - today's
+    # Do this since tweets likely show effect in the future
     tesla_data['Changes'] = tesla_data['Close'] - tesla_data['Close'].shift(-1)
 
     sentiment_data = pd.read_csv('tweets_classifed_using_classifier.csv')
@@ -41,5 +45,26 @@ if __name__ == '__main__':
 
     sentiment_data_count = pd.DataFrame({'count' : sentiment_data.groupby( [ "Date", "Classification"] ).size()}).reset_index()
     sentiment_data_count = sentiment_data_count.pivot(index='Date', columns='Classification', values='count').reset_index()
-    
-    print(sentiment_data_count.merge(tesla_data, left_on='Date', right_on='Date'))
+
+    # print(sentiment_data_count.merge(tesla_data, left_on='Date', right_on='Date'))
+    final_data = sentiment_data_count.merge(tesla_data, left_on='Date', right_on='Date')
+    final_data = final_data.rename(index=str, columns={-1: 'Negative', 0: 'Neutral', 1: 'Positive'})
+
+    # removes NaN values
+    final_data = final_data[final_data['Changes'] == final_data['Changes']]
+    print(final_data)
+
+    # Build the regressor
+    y = final_data['Changes']
+    X = final_data[['Positive', 'Neutral', 'Negative']]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+
+    regressor = LinearRegression()
+    regressor.fit(X_train, y_train)
+    print(regressor.score(X_test, y_test))
+
+
+
+
+
+
