c08bb71259f9b7eae100a5a635fde9aea594b62b
itabr <itabrz@gmail.com>
2017-12-08 22:49:37 -0800

    tweets_classifed_using_classifier.csv added

--- code/tweet_class.py
+++ code/tweet_class.py
@@ -4,8 +4,8 @@ import nltk
 from nltk.tokenize import word_tokenize
 import re
 
-def featureListgenerator(f):
-	reader = csv.reader(f)
+def tweet_trainer(f_training):
+	reader = csv.reader(f_training)
 	tweets = []
 	for row in reader:
 		sentiment = row[0]
@@ -16,17 +16,33 @@ def featureListgenerator(f):
 	all_words = set(word.lower() for tweet in tweets for word in word_tokenize(tweet[0]))
 	t = [({word: (word in word_tokenize(x[0])) for word in all_words}, x[1]) for x in tweets]
 	classifier = nltk.NaiveBayesClassifier.train(t)
-	classifier.show_most_informative_features()
+	#classifier.show_most_informative_features()
 
-	test_sentence = "tesla stock price is going down bad terible"
-	test_sent_features = {word.lower(): (word in word_tokenize(test_sentence.lower())) for word in all_words}
-
-	print(classifier.classify(test_sent_features))
+	return (classifier,all_words)
 
 
+def tweet_classyfier(f_testing,f_output,classifier,all_words):
+	reader_testing = csv.reader(f_testing)
+	for row in reader_testing:
+		timestamp = row[0]
+		processedTweet = clean_tweet(row[1])
+		test_sent_features = {word.lower(): (word in word_tokenize(processedTweet.lower())) for word in all_words}
+		sentiment = classifier.classify(test_sent_features)
+		
+		line = sentiment + "," + timestamp + "," + processedTweet
+		f_output.write(line)
+		f_output.write('\n')
+
+		print(sentiment,timestamp,processedTweet)
 
 
 
-f = open("../tweet_training_tesla.csv", 'r', encoding='mac_roman', newline='')
 
-featureListgenerator(f)
+
+f_training = open("../tweet_training_tesla.csv", 'r', encoding='mac_roman', newline='')
+f_testing = open("../tweets_tesla.csv", 'r', encoding='mac_roman', newline='')
+f_output = open("tweets_classifed_using_classifier.csv","w+")
+
+train = tweet_trainer(f_training)
+
+tweet_classyfier(f_testing,f_output,train[0],train[1])
