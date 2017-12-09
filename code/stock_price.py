3b67ee9d8482109f50a5c8d8339b3d9d369b2606
Nilesh C. Gupta <nilesh@risksciences.ucla.edu>
2017-12-08 22:26:00 -0800

    TSLA stock data 11/28 to 12/8

--- code/stock_price.py
+++ code/stock_price.py
@@ -14,19 +14,18 @@ def google_stocks(symbol, startdate = (1, 1, 2006), enddate = None):
         enddate = str(enddate[0]) + '+' + str(enddate[1]) + '+' + str(enddate[2])
 
     stock_url = "http://finance.google.com/finance/historical?q=" + symbol + \
-    			"&startdate=" + startdate + "&enddate=" + enddate + "&output=csv"
+                "&startdate=" + startdate + "&enddate=" + enddate + "&output=csv"
 
     print stock_url
  
     raw_response = requests.get(stock_url).content
- 	
+    
     stock_data = pd.read_csv(io.StringIO(raw_response.decode('utf-8')))
     stock_data['Date'] = stock_data['Date'].apply(lambda x: datetime.strptime(x, '%d-%b-%y').date())
     return stock_data
 
-# if __name__ == '__main__':
-#     apple_data = google_stocks('AAPL', startdate=(11, 1, 2006))
-#     print(apple_data)
- 
-    # apple_truncated = google_stocks('AAPL', enddate = (1, 1, 2006))
-    # print(apple_truncated)
+if __name__ == '__main__':
+    tesla_data = google_stocks('TSLA', startdate=(11, 26, 2017), enddate=(12, 9, 2017))
+    tesla_data = tesla_data[['Date', 'Close']]
+    tesla_data['diff Close'] = tesla_data['Close'] - tesla_data['Close'].shift(-1)
+    print(tesla_data)
