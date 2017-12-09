c1581b9c32cbe79e92f8ba98d1949ce0ac32f13a
Nilesh C. Gupta <nilesh@risksciences.ucla.edu>
2017-12-09 02:05:33 -0800

    Preprocessed stock and twitter data to get joint dataframe

--- code/stock_price.py
+++ code/stock_price.py
@@ -22,10 +22,24 @@ def google_stocks(symbol, startdate = (1, 1, 2006), enddate = None):
     
     stock_data = pd.read_csv(io.StringIO(raw_response.decode('utf-8')))
     stock_data['Date'] = stock_data['Date'].apply(lambda x: datetime.strptime(x, '%d-%b-%y').date())
+    # stock_data['Date'] = stock_data['Date'].apply(lambda x: str(x))
     return stock_data
 
 if __name__ == '__main__':
-    tesla_data = google_stocks('TSLA', startdate=(11, 26, 2017), enddate=(12, 9, 2017))
+    tesla_data = google_stocks('TSLA', startdate=(11, 28, 2017), enddate=(12, 11, 2017))
     tesla_data = tesla_data[['Date', 'Close']]
-    tesla_data['diff Close'] = tesla_data['Close'] - tesla_data['Close'].shift(-1)
-    print(tesla_data)
+    tesla_data['Changes'] = tesla_data['Close'] - tesla_data['Close'].shift(-1)
+
+    sentiment_data = pd.read_csv('tweets_classifed_using_classifier.csv')
+    sentiment_data.columns = ['Classification', 'Date', 'Tweet']
+    sentiment_data = sentiment_data.drop('Tweet', axis=1)
+
+    for index, row in sentiment_data.iterrows():
+        datestring = row['Date'][4:10] + " " + row['Date'][-4:]
+        datetime_object = datetime.strptime(datestring, '%b %d %Y')
+        sentiment_data.at[index, 'Date'] = datetime_object.date()
+
+    sentiment_data_count = pd.DataFrame({'count' : sentiment_data.groupby( [ "Date", "Classification"] ).size()}).reset_index()
+    sentiment_data_count = sentiment_data_count.pivot(index='Date', columns='Classification', values='count').reset_index()
+    
+    print(sentiment_data_count.merge(tesla_data, left_on='Date', right_on='Date'))
